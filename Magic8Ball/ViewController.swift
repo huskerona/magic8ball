8c8094be0b0d96cdf636bbe5b22ed5be815cb1fa
Husein Roncevic <huseinr@outlook.com>
2020-11-26 17:24:14 +0100

    almost finished app

--- Magic8Ball/ViewController.swift
+++ Magic8Ball/ViewController.swift
@@ -8,12 +8,22 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var messageImageView: UIImageView!
+    
+    let messageName: String = "Message-"
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func askTheBallButton(_ sender: Any) {
+        let num = Int.random(in: 1...15)
+        let message = messageName + "\(num)"
+        
+        messageImageView.image = UIImage(named: message)
+    }
+    
 }
 
